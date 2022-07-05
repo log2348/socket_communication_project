5d2aa001da07d668b6081f5dfbbf2b369c9773b3
Jihyeon Park <96460131+log2348@users.noreply.github.com>
2022-07-05 22:07:04 +0900

    first commit

---
+++ src/small_talk/ChatService.java
@@ -0,0 +1,15 @@
+package small_talk;
+
+public interface ChatService {
+	
+	void chat();
+	void whisper();
+	void createRoom();
+	void enterRoom();
+	void exitRoom();
+	void newRoom();
+	void newUser();
+	void oldRoom();
+	void oldUsesr();
+
+}
